8c683c04eb5f14b26958ca66c6cdd007e41f4b8c
gustta11 <gustavokeven50@gmail.com>
2025-12-10 15:40:39 -0300

    Estudo modificadores estáticos

---
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/test/CarroTest01.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.javacore.Fmodificadorestatico.test;
+
+import academy.devdojo.maratonajava.javacore.Fmodificadorestatico.dominio.Carro;
+
+public class CarroTest01 {
+    public static void main(String[] args) {
+        Carro carro01 = new Carro("BMW", 200);
+        Carro carro02 = new Carro("Audi", 300);
+
+        Carro.velocidadeLimite = 100;
+        carro01.imprime();
+        carro02.imprime();
+
+    }
+}
