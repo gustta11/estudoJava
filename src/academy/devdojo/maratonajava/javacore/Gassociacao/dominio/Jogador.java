bc64e8a1853e2c507be355b583317bc7f86463aa
gustta11 <gustavokeven50@gmail.com>
2025-12-15 22:37:44 -0300

    Estudo sobre associação

---
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/dominio/Jogador.java
@@ -0,0 +1,36 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.dominio;
+
+public class Jogador {
+
+    private String nome;
+    private int idade;
+    private Time time;
+
+    public void imprime(){
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(time.getNome());
+    }
+
+    public Jogador(String nome, int idade, Time time){
+        this.nome = nome;
+        this.idade = idade;
+        this.time = time;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+
+    public int getIdade(){
+        return idade;
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/test/JogadorTest01.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.test;
+
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Jogador;
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Time;
+
+public class JogadorTest01 {
+    public static void main(String[] args) {
+        Time time = new Time("Santos");
+        Time time2 = new Time("Bahia");
+        Jogador jogador1 = new Jogador("Cafu", 18, time);
+        Jogador jogador2 = new Jogador("Elias", 25, time2);
+        Jogador jogador3 = new Jogador("Neymar", 22, time);
+
+        Jogador[] jogadores = {jogador1,jogador2,jogador3};
+
+        for(Jogador jogador : jogadores){
+            jogador.imprime();
+        }
+    }
+
+}
